e18231015d555203d03352e14e13a982ec0430dc
cptspacecadet <m@dao-lawfirm.xyz>
2022-09-19 14:26:24 -0400

    - deployer contract for NFToken

---
+++ contracts/extensions/Deployer/Deployer_v001.sol
@@ -0,0 +1,59 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import '@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol';
+// import {Initializable as InitializableUpgradeable} from '@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol';
+import '@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol';
+import '@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol';
+
+import '../../interfaces/IJBDirectory.sol';
+import './NFTokenFactory.sol';
+
+/**
+ * @notice
+ */
+/// @custom:oz-upgrades-unsafe-allow external-library-linking
+contract Deployer_v001 is Initializable, OwnableUpgradeable, UUPSUpgradeable {
+  event Deployment(string contractType, address contractAddress);
+
+  function initialize() public initializer {
+    __Ownable_init();
+    __UUPSUpgradeable_init();
+  }
+
+  function _authorizeUpgrade(address) internal override onlyOwner {}
+
+  function deployNFToken(
+    address _owner,
+    string memory _name,
+    string memory _symbol,
+    string memory _baseUri,
+    string memory _contractUri,
+    uint256 _jbxProjectId,
+    IJBDirectory _jbxDirectory,
+    uint256 _maxSupply,
+    uint256 _unitPrice,
+    uint256 _mintAllowance,
+    uint128 _mintPeriodStart,
+    uint128 _mintPeriodEnd
+  ) external returns (address) {
+    address t = NFTokenFactory.createNFToken(
+      _owner,
+      _name,
+      _symbol,
+      _baseUri,
+      _contractUri,
+      _jbxProjectId,
+      _jbxDirectory,
+      _maxSupply,
+      _unitPrice,
+      _mintAllowance,
+      _mintPeriodStart,
+      _mintPeriodEnd
+    );
+
+    emit Deployment('NFToken', t);
+
+    return t;
+  }
+}
