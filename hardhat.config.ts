241fdeb60ab74c998b6f63644da1499ed6d970fd
cptspacecadet <m@dao-lawfirm.xyz>
2022-10-11 13:16:06 -0400

    - downgrade solidity due to solidity-coverage #753

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -77,7 +77,7 @@ module.exports = {
         },
     },
     solidity: {
-        version: '0.8.17',
+        version: '0.8.14',
         settings: {
             optimizer: {
                 enabled: true,
