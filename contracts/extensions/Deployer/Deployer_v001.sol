7341ee8d2e7c1bc88e2d6c863254a42d03576352
cptspacecadet <m@dao-lawfirm.xyz>
2022-12-20 21:39:46 -0500

    - WIP: fee & discounts integration

--- contracts/extensions/Deployer/Deployer_v001.sol
+++ contracts/extensions/Deployer/Deployer_v001.sol
@@ -4,30 +4,53 @@ pragma solidity ^0.8.0;
 import '@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol';
 import '@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol';
 
+import '../../abstract/JBOperatable.sol';
+import '../../libraries/JBOperations.sol';
 import '../../interfaces/IJBDirectory.sol';
+import '../../interfaces/IJBProjects.sol';
+import '../../interfaces/IJBOperatorStore.sol';
+import '../PlatformDiscountManager.sol';
 import './Factories/NFTokenFactory.sol';
 
 /**
  * @notice
  */
 /// @custom:oz-upgrades-unsafe-allow external-library-linking
-contract Deployer_v001 is OwnableUpgradeable, UUPSUpgradeable {
+contract Deployer_v001 is JBOperatable, OwnableUpgradeable, UUPSUpgradeable {
   event Deployment(string contractType, address contractAddress);
 
   error INVALID_PAYMENT(uint256 price);
+  error INVALID_ADDRESS();
+  error INVALID_AMOUNT();
 
-  bytes32 internal deployNFTokenKey = keccak256(abi.encodePacked('deployNFToken'));
+  // IJBDirectory public jbxDirectory
+  // IJBProjects public jbxProjects;
+
+  PlatformDiscountManager public platformDiscountManager;
 
   mapping(bytes32 => uint256) public prices;
+
+  bytes32 internal constant deployNFTokenKey = keccak256(abi.encodePacked('deployNFToken'));
 
   /// @custom:oz-upgrades-unsafe-allow constructor
   constructor() {
     _disableInitializers();
   }
 
-  function initialize() public virtual initializer {
+  function initialize()
+    public
+    virtual
+    // IJBDirectory _jbxDirectory,
+    // IJBProjects _jbxProjects,
+    // IJBOperatorStore _jbxOperatorStore
+    initializer
+  {
     __Ownable_init();
     __UUPSUpgradeable_init();
+
+    // operatorStore = _jbxOperatorStore;
+    // jbxDirectory = _jbxDirectory;
+    // jbxProjects = _jbxProjects;
 
     prices[deployNFTokenKey] = 1000000000000000; // 0.001 eth
   }
@@ -48,7 +71,7 @@ contract Deployer_v001 is OwnableUpgradeable, UUPSUpgradeable {
     uint256 _mintPeriodStart,
     uint256 _mintPeriodEnd
   ) external payable returns (address) {
-    validatePayment();
+    validatePayment(deployNFTokenKey);
 
     address t = NFTokenFactory.createNFToken(
       _owner,
@@ -70,10 +93,54 @@ contract Deployer_v001 is OwnableUpgradeable, UUPSUpgradeable {
     return t;
   }
 
-  function validatePayment() internal {
-    uint256 price = prices[deployNFTokenKey];
+  function validatePayment(bytes32 _priceKey) internal virtual {
+    uint256 price = prices[_priceKey];
+
+    if (price == 0) {
+      return;
+    }
+
+    if (address(platformDiscountManager) != address(0)) {
+      price = platformDiscountManager.getPrice(msg.sender, price);
+    }
+
     if (msg.value != price) {
       revert INVALID_PAYMENT(price);
     }
+  }
+
+  function updatePrice(bytes32 _priceKey, uint256 _price) external virtual {
+    // TODO: permissions
+    prices[_priceKey] = _price;
+  }
+
+  //   function transferBalance(
+  //     address payable _destination,
+  //     uint256 _amount
+  //   )
+  //     external
+  //     nonReentrant
+  //     requirePermissionAllowingOverride(
+  //       jbxProjects.ownerOf(jbxProjectId),
+  //       jbxProjectId,
+  //       JBOperations.MANAGE_PAYMENTS,
+  //       (msg.sender == address(jbxDirectory.controllerOf(jbxProjectId)))
+  //     )
+  //   {
+  //     // TODO: permissions
+  //     if (_destination == address(0)) {
+  //       revert INVALID_ADDRESS();
+  //     }
+
+  //     if (_amount == 0 || _amount > (payable(address(this))).balance) {
+  //       revert INVALID_AMOUNT();
+  //     }
+
+  //     _destination.transfer(_amount);
+  //   }
+
+  function setPlatformDiscountManager(PlatformDiscountManager _platformDiscountManager) external {
+    // TODO: permissions
+    // platformDiscountManager = _platformDiscountManager;
   }
 }
