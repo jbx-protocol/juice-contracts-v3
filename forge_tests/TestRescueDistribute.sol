e67d2a0277169a4dd4af8d24c98385646c33b3b1
drgorillamd <dr_gorilla@protonmail.com>
2023-02-09 23:30:18 +0100

    mor balances tests

--- forge_tests/TestRescueDistribute.sol
+++ forge_tests/TestRescueDistribute.sol
@@ -93,6 +93,7 @@ contract TestRescueDistribute_Fork is Test {
         // Some sanity check
         require(jbDirectory == newJbController.directory(), "Setup: directories mismatch");
         require(jbDirectory.controllerOf(1) == address(newJbController), "Setup: new controller mismatch");
+
         _createFundingCycleData();
     }
 
@@ -152,6 +153,8 @@ contract TestRescueDistribute_Fork is Test {
         _targets[1] = address(jbEthTerminal);
         _targets[2] = address(jbDirectory);
 
+        // ---- Execution -----
+
         // Deploy the batching contract as a template
         FakeMultisigBatcher _multisig = new FakeMultisigBatcher();
 
@@ -171,66 +174,28 @@ contract TestRescueDistribute_Fork is Test {
         // Check: controller back to the new one?
         assertEq(jbDirectory.controllerOf(1), address(newJbController));
 
-        // Check: project balance decreased in the terminal store (of an amount between all and 0 fee-less recipient)
+        // Check: project balance decreased in the terminal store (74.1% of the recipient are within the ecosystem)
         assertGe(_projectBalanceAfter, _projectBalance - _ethAmountDistributed);
-        assertLe(_projectBalanceAfter, _projectBalance - (_ethAmountDistributed * 975 / 1000));
+        assertLe(_projectBalanceAfter, _projectBalance - (_ethAmountDistributed * 259 / 1000));
 
         // Check: terminal ETH balance decreased?
         assertGe(_terminalBalanceAfter, _terminalBalance - _ethAmountDistributed);
-        assertLe(_terminalBalanceAfter, _terminalBalance - (_ethAmountDistributed * 975 / 1000));
+        assertLe(_terminalBalanceAfter, _terminalBalance - (_ethAmountDistributed * 259 / 10000));
 
-        // Check: reserved token balance in the v3.1 unchanged?
-        assertEq(_reservedTokenV3_1After, _reservedTokenV3_1);
+        // // Check: reserved token balance in the v3.1 unchanged?
+        // assertEq(_reservedTokenV3_1After, _reservedTokenV3_1);
 
-        // Check: reserved token balance in the v3 unchanged?
-        assertEq(_reservedTokenV3After, _reservedTokenV3);
+        // // Check: reserved token balance in the v3 unchanged?
+        // assertEq(_reservedTokenV3After, _reservedTokenV3);
     }
 
-    function _migrate(uint256 _projectId) internal returns (JBController3_0_1 jbController) {
-        return _migrateWithGroupedsplits(_projectId, new JBGroupedSplits[](0));
-    }
+    ////////////////////////////////////////////////////////////////////
+    //                            helpers                             //
+    ////////////////////////////////////////////////////////////////////
 
     /**
-     * @notice  Create a new controller, set a new fc with the allowControllerMigration flag set to true
-     *          then warp and migrate the project to the new controller
-     * @param   _projectId      The id of the project to migrate
-     * @param   _groupedSplits  A grouped splits for the reserved tokens
-     * @return  jbController    The new controller
+     * @notice Assign some classic values to the funding cycle data globally available in this test
      */
-    function _migrateWithGroupedsplits(uint256 _projectId, JBGroupedSplits[] memory _groupedSplits)
-        internal
-        returns (JBController3_0_1 jbController)
-    {
-        // Create a new controller
-        jbController = new JBController3_0_1(
-            jbOperatorStore,
-            jbProjects,
-            jbDirectory,
-            jbFundingCycleStore,
-            jbTokenStore,
-            jbSplitsStore
-        );
-
-        address _projectOwner = jbProjects.ownerOf(_projectId);
-
-        // Allow controller migration in the fc
-        metadata.allowControllerMigration = true;
-
-        vm.prank(_projectOwner);
-        oldJbController.reconfigureFundingCyclesOf(
-            _projectId, data, metadata, 0, _groupedSplits, fundAccessConstraints, ""
-        );
-
-        // warp to the next funding cycle
-        JBFundingCycle memory fundingCycle = jbFundingCycleStore.currentOf(_projectId);
-        vm.warp(fundingCycle.start + (fundingCycle.duration) * 2); // skip 2 fc to avoid ballot
-
-        // Migrate the project to the new controller (no prepForMigration(..) needed anymore)
-        vm.prank(_projectOwner);
-        oldJbController.migrate(_projectId, jbController);
-    }
-
-
     function _createFundingCycleData() internal  {
         // Set some mock fc data
         projectMetadata = JBProjectMetadata({content: "myIPFSHash", domain: 1});
@@ -280,14 +245,19 @@ contract TestRescueDistribute_Fork is Test {
             })
         );
     }
-
-    function _getReservedRate(uint256 _projectId) internal returns(uint256) {
+    
+    /**
+     * @notice Get the reserved rate of the project (stack management)
+     */
+    function _getReservedRate(uint256 _projectId) internal view returns(uint256) {
         JBFundingCycle memory fundingCycle = jbFundingCycleStore.currentOf(1);
         return fundingCycle.reservedRate();
     }
 }
 
-
+/**
+ * @notice Simulate a batched transaction, same as what a multisig would do
+ */
 contract FakeMultisigBatcher {
     // This...this has been written by copilot, entirely!
     function exec(address[] memory _targets, bytes[] memory _datas) public {
