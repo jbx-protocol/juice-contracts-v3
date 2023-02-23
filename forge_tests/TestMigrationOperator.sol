22f5c28576ff107220627970c8fba01c0d28d10d
DrGorilla_md <dr_gorilla@protonmail.com>
2023-02-23 12:34:29 +0100

    tests

--- forge_tests/TestMigrationOperator.sol
+++ forge_tests/TestMigrationOperator.sol
@@ -129,8 +129,6 @@ contract TestMigrationOperator_Local is TestBaseWorkflow {
             projectId, address(_newJbController), jbEthTerminal3_1, jbETHPaymentTerminal()
         );
 
-        // TODO: checks
-
         // Check: the project must have the new controller
         assertEq(jbDirectory().controllerOf(projectId), address(_newJbController));
 
@@ -331,11 +329,101 @@ contract TestMigrationOperator_Fork is Test {
         jbTerminalStore = jbEthTerminal.store();
 
         // Set some mock fc data
+        _initMetadata();
+
+        migrationOperator = new JBMigrationOperator(jbDirectory);
+    }
+
+    /**
+     * @notice  Test if a project can migrate its controller and terminals using the migrator
+     * @dev     The project must have a controller and the correct permission sets in fc and
+     * operator store
+     *          JuiceboxDAO (id 1) is already using Controller 3.0.1 at that block height
+     */
+    function testMigrationOperator_migrateAnyExistingProject(uint256 _projectId) public {
+        // Migrate only existing projects
+        _projectId = bound(_projectId, 1, jbProjects.count());
+
+        // Migrate only project which are not archived/have a controller
+        vm.assume(jbDirectory.controllerOf(_projectId) != address(0));
+
+        // JuiceboxDAO is already using Controller 3.0.1 at that block height
+        if (_projectId == 1) oldJbController = IJBController(jbDirectory.controllerOf(1));
+
+        _prepareAuthorizations(_projectId);
+
+        vm.prank(jbProjects.ownerOf(_projectId));
+        migrationOperator.migrate(
+            _projectId, address(newJbController), jbEthPaymentTerminal3_1, jbEthTerminal
+        );
+
+        // Check: the project must have the new controller
+        assertEq(jbDirectory.controllerOf(_projectId), address(newJbController));
+
+        // Check: the project must use the new terminal as primary terminal
+        assertEq(
+            address(jbDirectory.primaryTerminalOf(_projectId, JBTokens.ETH)),
+            address(jbEthPaymentTerminal3_1)
+        );
+    }
+
+    ////////////////////////////////////////////////////////////////////
+    //                            Helpers                             //
+    ////////////////////////////////////////////////////////////////////
+
+    /**
+     * @notice  Set the correct premigration authorizations in the operator store and fc metadata
+     */
+    function _prepareAuthorizations(uint256 _projectId) internal {
+        address _owner = jbProjects.ownerOf(_projectId);
+
+        // Set the correct permissions in the operator store
+        uint256[] memory _permissionIndexes = new uint256[](3);
+        _permissionIndexes[0] = JBOperations.MIGRATE_CONTROLLER;
+        _permissionIndexes[1] = JBOperations.MIGRATE_TERMINAL;
+        _permissionIndexes[2] = JBOperations.SET_TERMINALS;
+
+        vm.prank(_owner);
+        jbOperatorStore.setOperator(
+            JBOperatorData({
+                operator: address(migrationOperator),
+                domain: _projectId,
+                permissionIndexes: _permissionIndexes
+            })
+        );
+
+        // Set the correct permissions in the funding cycle metadata
+        metadata.allowControllerMigration = true;
+        metadata.allowTerminalMigration = true;
+        metadata.global.allowSetTerminals = true;
+
+        // Reconfigure the project's fc
+        vm.prank(_owner);
+        oldJbController.reconfigureFundingCyclesOf(
+            _projectId, data, metadata, 0, groupedSplits, fundAccessConstraints, ""
+        );
+
+        // warp to the next funding cycle
+        JBFundingCycle memory fundingCycle = jbFundingCycleStore.currentOf(_projectId);
+
+        vm.warp(
+            fundingCycle.duration == 0
+                ? fundingCycle.ballot != IJBFundingCycleBallot(address(0))
+                    ? block.timestamp + fundingCycle.ballot.duration() + 1
+                    : block.timestamp + 1
+                : fundingCycle.start + fundingCycle.duration * 2 // skip 2 fc to easily avoid ballot
+        );
+    }
+
+    /**
+     * @notice  Initialize the funding cycle and fund access constraints data to some generic values
+     */
+    function _initMetadata() internal {
         projectMetadata = JBProjectMetadata({content: "myIPFSHash", domain: 1});
 
         data = JBFundingCycleData({
             duration: 14,
-            weight: weight,
+            weight: 10 ether,
             discountRate: 0,
             ballot: IJBFundingCycleBallot(address(0))
         });
@@ -365,300 +453,17 @@ contract TestMigrationOperator_Fork is Test {
             metadata: 0
         });
 
-        terminals.push(jbEthTerminal);
+        terminals.push(jbETHPaymentTerminal());
 
         fundAccessConstraints.push(
             JBFundAccessConstraints({
-                terminal: jbEthTerminal,
+                terminal: jbETHPaymentTerminal(),
                 token: JBTokens.ETH,
-                distributionLimit: targetInWei, // 10 ETH target
+                distributionLimit: 10 ether,
                 overflowAllowance: 5 ether,
                 distributionLimitCurrency: 1, // Currency = ETH
                 overflowAllowanceCurrency: 1
             })
-        );
-
-        migrationOperator = new JBMigrationOperator(jbDirectory);
-    }
-
-    ////////////////////////////////////////////////////////////////////
-    //                      migrate(..) flow                          //
-    ////////////////////////////////////////////////////////////////////
-
-    /**
-     * @notice  Test if a project using the controller V3 can be migrated to the controller V3.1
-     * @dev     The project must have a controller, not archived and the allowControllerMigration
-     * flag must be set
-     */
-    function testMigrationOperator_migrateAnyExistingProject(uint256 _projectId) public {
-        // Migrate only existing projects
-        _projectId = bound(_projectId, 1, jbProjects.count());
-
-        // Migrate only project which are not archived/have a controller
-        vm.assume(jbDirectory.controllerOf(_projectId) != address(0));
-
-        // JuiceboxDAO is already using Controller 3.0.1 at that block height
-        if (_projectId == 1) oldJbController = IJBController(jbDirectory.controllerOf(1));
-
-        _prepareAuthorizations(_projectId);
-
-        vm.prank(jbProjects.ownerOf(_projectId));
-        migrationOperator.migrate(
-            _projectId, address(newJbController), jbEthPaymentTerminal3_1, jbEthTerminal
-        );
-
-        // Check: the project must have the new controller
-        assertEq(jbDirectory.controllerOf(_projectId), address(newJbController));
-
-        // Check: the project must use the new terminal as primary terminal
-        assertEq(
-            address(jbDirectory.primaryTerminalOf(_projectId, JBTokens.ETH)),
-            address(jbEthPaymentTerminal3_1)
-        );
-    }
-
-    //   /**
-    //    * @notice Test if migrating a project with a reserved token will distribute the reserved
-    // token before migrating
-    //    */
-    //   function testController31_Migration_distributeReservedTokenBeforeMigrating() external {
-    //     address _projectOwner = makeAddr('_projectOwner');
-    //     address _userWallet = makeAddr('_userWallet');
-
-    //     uint256 _reservedRate = 4000; // 40%
-    //     uint256 n_reserved_split = 5;
-
-    //     // Configure the grouped splits
-    //     JBSplit[] memory _split = new JBSplit[](n_reserved_split);
-    //     for (uint256 i = 0; i < n_reserved_split; i++) {
-    //       address _user = vm.addr(i + 1);
-    //       _split[i] = JBSplit({
-    //         preferClaimed: false,
-    //         preferAddToBalance: false,
-    //         percent: JBConstants.SPLITS_TOTAL_PERCENT / n_reserved_split,
-    //         projectId: 0,
-    //         beneficiary: payable(_user),
-    //         lockedUntil: 0,
-    //         allocator: IJBSplitAllocator(address(0))
-    //       });
-    //     }
-    //     JBGroupedSplits[] memory _groupedSplits = new JBGroupedSplits[](1);
-    //     _groupedSplits[0] = JBGroupedSplits({group: JBSplitsGroups.RESERVED_TOKENS, splits:
-    // _split});
-
-    //     // Create a project with a reserved rate to insure the project has undistributed reserved
-    // tokens
-    //     metadata.reservedRate = _reservedRate;
-    //     uint256 _projectId = oldJbController.launchProjectFor(
-    //       _projectOwner,
-    //       projectMetadata,
-    //       data,
-    //       metadata,
-    //       block.timestamp,
-    //       _groupedSplits,
-    //       fundAccessConstraints,
-    //       terminals,
-    //       ''
-    //     );
-
-    //     // Avoid overwriting the fc when reconfiguring the project
-    //     vm.warp(block.timestamp + 1);
-
-    //     // Pay the project, 40% are reserved
-    //     uint256 payAmountInWei = 10 ether;
-    //     jbEthTerminal.pay{value: payAmountInWei}(
-    //       _projectId,
-    //       payAmountInWei,
-    //       address(0),
-    //       _userWallet,
-    //       /* _minReturnedTokens */
-    //       0,
-    //       /* _preferClaimedTokens */
-    //       false,
-    //       /* _memo */
-    //       'Take my money!',
-    //       /* _delegateMetadata */
-    //       new bytes(0)
-    //     );
-
-    //     // Check: Weight is 1-1, so the reserved tokens are 40% of the gross pay amount
-    //     assertEq(
-    //       oldJbController.reservedTokenBalanceOf(_projectId, _reservedRate),
-    //       (payAmountInWei * _reservedRate) / JBConstants.MAX_RESERVED_RATE
-    //     );
-
-    //     // Migrate the controller to v3_1
-    //     JBController3_0_1 jbController = _migrateWithGroupedsplits(_projectId, _groupedSplits);
-
-    //     // Check: Assert that the reserved tokens have been distributed and can no longer be
-    // distributed
-    //     assertEq(oldJbController.reservedTokenBalanceOf(_projectId, _reservedRate), 0);
-    //     assertEq(jbController.reservedTokenBalanceOf(_projectId), 0);
-
-    //     // Check: Assert that all users in the split received their share
-    //     for (uint256 i = 0; i < n_reserved_split; i++) {
-    //       address _user = vm.addr(i + 1);
-    //       assertEq(
-    //         jbController.tokenStore().unclaimedBalanceOf(_user, _projectId),
-    //         (payAmountInWei * _reservedRate) / JBConstants.MAX_RESERVED_RATE / n_reserved_split
-    //       );
-    //     }
-    //   }
-
-    //   /**
-    //    * @notice Test if a project using the controller V3.1 has the reserved token balance
-    // adequalty tracked
-    //    */
-    //   function testController31_Migration_tracksReservedTokenInNewController(uint8 _projectId)
-    //     external
-    //   {
-    //     // Migrate only existing projects
-    //     vm.assume(_projectId <= jbProjects.count() && _projectId > 1);
-
-    //     // Migrate only project which are not archived/have a controller
-    //     vm.assume(jbDirectory.controllerOf(_projectId) != address(0));
-
-    //     address _userWallet = makeAddr('_userWallet');
-
-    //     metadata.reservedRate = 4000; // 40%
-
-    //     JBController3_0_1 jbController = _migrate(_projectId);
-
-    //     // No reserved token before any transaction
-    //     assertEq(jbController.reservedTokenBalanceOf(_projectId), 0);
-
-    //     // Pay the project, 40% are reserved
-    //     uint256 payAmountInWei = 10 ether;
-    //     jbEthTerminal.pay{value: payAmountInWei}(
-    //       _projectId,
-    //       payAmountInWei,
-    //       address(0),
-    //       _userWallet,
-    //       /* _minReturnedTokens */
-    //       1,
-    //       /* _preferClaimedTokens */
-    //       false,
-    //       /* _memo */
-    //       'Take my money!',
-    //       /* _delegateMetadata */
-    //       new bytes(0)
-    //     );
-
-    //     // Check: Weight is 1-1, so the reserved tokens are 40% of the gross pay amount
-    //     assertEq(
-    //       jbController.reservedTokenBalanceOf(_projectId),
-    //       (payAmountInWei * 4000) / JBConstants.MAX_RESERVED_RATE
-    //     );
-    //   }
-
-    //   /**
-    //    * @notice  Test if the new controller might launch new projects
-    //    * @dev     The controller need to be allowed to set a new controller in new projects, in
-    // JBDirectory
-    //    */
-    //   function testController31_Migration_launchNewProjectViaNewController(uint256 _reservedRate)
-    //     external
-    //   {
-    //     // Pass only valid reserved rates
-    //     _reservedRate = bound(_reservedRate, 0, JBConstants.MAX_RESERVED_RATE);
-
-    //     address _userWallet = makeAddr('_userWallet');
-    //     address _projectOwner = makeAddr('projectOwner');
-    //     address _protocolOwner = jbProjects.ownerOf(1);
-
-    //     // Create a new controller
-    //     JBController3_0_1 _jbController = new JBController3_0_1(
-    //       jbOperatorStore,
-    //       jbProjects,
-    //       jbDirectory,
-    //       jbFundingCycleStore,
-    //       jbTokenStore,
-    //       jbSplitsStore
-    //     );
-
-    //     // Grant the permission to the new controller to launch a project, in the directory
-    //     vm.prank(_protocolOwner);
-    //     jbDirectory.setIsAllowedToSetFirstController(address(_jbController), true);
-
-    //     // Create a project with a reserved rate to insure the project has undistributed reserved
-    // tokens
-    //     metadata.reservedRate = _reservedRate;
-
-    //     uint256 _projectId = _jbController.launchProjectFor(
-    //       _projectOwner,
-    //       projectMetadata,
-    //       data,
-    //       metadata,
-    //       block.timestamp,
-    //       groupedSplits,
-    //       fundAccessConstraints,
-    //       terminals,
-    //       ''
-    //     );
-
-    //     // Check: Assert that the project has been created
-    //     assertTrue(_projectId > 0);
-
-    //     // Pay the project, 40% are reserved
-    //     uint256 payAmountInWei = 10 ether;
-    //     jbEthTerminal.pay{value: payAmountInWei}(
-    //       _projectId,
-    //       payAmountInWei,
-    //       address(0),
-    //       _userWallet,
-    //       /* _minReturnedTokens */
-    //       0,
-    //       /* _preferClaimedTokens */
-    //       false,
-    //       /* _memo */
-    //       'Take my money!',
-    //       /* _delegateMetadata */
-    //       new bytes(0)
-    //     );
-
-    //     // Check: Weight is 1-1, so the reserved tokens are 40% of the gross pay amount
-    //     assertEq(
-    //       _jbController.reservedTokenBalanceOf(_projectId),
-    //       (payAmountInWei * _reservedRate) / JBConstants.MAX_RESERVED_RATE
-    //     );
-    //   }
-
-    // Set operator store and fc auth
-    function _prepareAuthorizations(uint256 _projectId) internal {
-        address _owner = jbProjects.ownerOf(_projectId);
-
-        uint256[] memory _permissionIndexes = new uint256[](3);
-        _permissionIndexes[0] = JBOperations.MIGRATE_CONTROLLER;
-        _permissionIndexes[1] = JBOperations.MIGRATE_TERMINAL;
-        _permissionIndexes[2] = JBOperations.SET_TERMINALS;
-
-        vm.prank(_owner);
-        jbOperatorStore.setOperator(
-            JBOperatorData({
-                operator: address(migrationOperator),
-                domain: _projectId,
-                permissionIndexes: _permissionIndexes
-            })
-        );
-
-        metadata.allowControllerMigration = true;
-        metadata.allowTerminalMigration = true;
-        metadata.global.allowSetTerminals = true;
-
-        vm.prank(_owner);
-        oldJbController.reconfigureFundingCyclesOf(
-            _projectId, data, metadata, 0, groupedSplits, fundAccessConstraints, ""
-        );
-
-        // warp to the next funding cycle
-        JBFundingCycle memory fundingCycle = jbFundingCycleStore.currentOf(_projectId);
-
-        vm.warp(
-            fundingCycle.duration == 0
-                ? fundingCycle.ballot != IJBFundingCycleBallot(address(0))
-                    ? block.timestamp + fundingCycle.ballot.duration() + 1
-                    : block.timestamp + 1
-                : fundingCycle.start + fundingCycle.duration * 2 // skip 2 fc to easily avoid ballot
         );
     }
 }
