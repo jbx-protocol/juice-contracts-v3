808a46286f8b1c6a4cb76624072e179224463d86
drgorillamd <dr_gorilla@protonmail.com>
2023-05-08 16:20:31 +0200

    fix: fix

--- deploy/v3_1.js
+++ deploy/v3_1.js
@@ -47,19 +47,6 @@ module.exports = async ({ deployments, getChainId }) => {
 
   console.log({ multisigAddress, protocolProjectStartsAtOrAfter });
 
-  // Deploy a JBETHERC20ProjectPayerDeployer contract.
-  await deploy('JBETHERC20ProjectPayerDeployer', {
-    ...baseDeployArgs,
-    args: [],
-  });
-
-  // Deploy a JBETHERC20SplitsPayerDeployer contract.
-  await deploy('JBETHERC20SplitsPayerDeployer', {
-    ...baseDeployArgs,
-    contract: 'contracts/JBETHERC20SplitsPayerDeployer.sol:JBETHERC20SplitsPayerDeployer',
-    args: [],
-  });
-
   // Deploy a JBOperatorStore contract.
   const JBOperatorStore = await deploy('JBOperatorStore', {
     ...baseDeployArgs,
@@ -189,6 +176,19 @@ module.exports = async ({ deployments, getChainId }) => {
       JBSingleTokenPaymentTerminalStore.address,
       multisigAddress,
     ],
+  });
+
+  // Deploy a JBETHERC20ProjectPayerDeployer contract.
+  await deploy('JBETHERC20ProjectPayerDeployer', {
+    ...baseDeployArgs,
+    args: [JBDirectory.address],
+  });
+
+  // Deploy a JBETHERC20SplitsPayerDeployer contract.
+  await deploy('JBETHERC20SplitsPayerDeployer', {
+    ...baseDeployArgs,
+    contract: 'contracts/JBETHERC20SplitsPayerDeployer.sol:JBETHERC20SplitsPayerDeployer',
+    args: [JBSplitStore.address],
   });
 
   // Get a reference to an existing ETH/USD feed.
