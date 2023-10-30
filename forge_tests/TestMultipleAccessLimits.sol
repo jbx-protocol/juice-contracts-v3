6db190d9eda3f3bfff02e4544719a787757ba5c1
nowonder <justinpulley92@gmail.com>
2023-10-29 21:10:53 -0500

    feat: more access limit tests

--- forge_tests/TestMultipleAccessLimits.sol
+++ forge_tests/TestMultipleAccessLimits.sol
@@ -134,6 +134,254 @@ contract TestMultipleDistLimits_Local is TestBaseWorkflow {
         vm.stopPrank();
     }
 
+    function testAccessConstraintsDelineation() external {
+        address _userWallet = address(1234);
+        uint256 _userPayAmount = 1.5 ether;
+
+        JBFundAccessConstraints3_1[] memory _fundAccessConstraints = new JBFundAccessConstraints3_1[](1);
+        JBCurrencyAmount[] memory _distributionLimits = new JBCurrencyAmount[](2);
+        JBCurrencyAmount[] memory _overflowAllowances = new JBCurrencyAmount[](1);
+
+        _distributionLimits[0] = JBCurrencyAmount({
+            value: 1 ether,
+            currency: jbLibraries().ETH()
+        });
+
+        _distributionLimits[1] = JBCurrencyAmount({
+            value: 2000000000,
+            currency: jbLibraries().USD()
+        });
+
+        _overflowAllowances[0] = JBCurrencyAmount({
+            value: 1,
+            currency: 1
+        });
+
+        _fundAccessConstraints[0] = 
+            JBFundAccessConstraints3_1({
+                terminal: _terminal3_2,
+                token: jbLibraries().ETHToken(),
+                distributionLimits: _distributionLimits,
+                overflowAllowances: _overflowAllowances
+            });
+
+        JBFundingCycleConfiguration[] memory _cycleConfig = new JBFundingCycleConfiguration[](1);
+
+        _cycleConfig[0].mustStartAtOrAfter = 0;
+        _cycleConfig[0].data = _data;
+        _cycleConfig[0].metadata = _metadata;
+        _cycleConfig[0].groupedSplits = _groupedSplits;
+        _cycleConfig[0].fundAccessConstraints = _fundAccessConstraints;
+
+        _projectOwner = multisig();
+
+        vm.prank(_projectOwner);
+        
+        _projectId = _controller.launchProjectFor(
+            _projectOwner,
+            _projectMetadata,
+            _cycleConfig,
+            _terminals,
+            ""
+        );
+
+        vm.deal(_userWallet, _userPayAmount);
+        vm.prank(_userWallet);
+
+        _terminal3_2.pay{value: _userPayAmount}(
+            _projectId,
+            _userPayAmount,
+            address(0),
+            _userWallet,
+            0,
+            false,
+            "Take my money!",
+            new bytes(0)
+        );
+
+        uint256 initTerminalBalance = address(_terminal3_2).balance;
+
+        // verify: beneficiary should have a balance of JBTokens
+        uint256 _userTokenBalance = PRBMathUD60x18.mul(_userPayAmount, _weight);
+        assertEq(_tokenStore.balanceOf(_userWallet, _projectId), _userTokenBalance);
+
+        vm.prank(_projectOwner);
+
+        // First dist meets our ETH limit (important below)
+        _terminal3_2.distributePayoutsOf(
+            _projectId,
+            1 ether,
+            jbLibraries().ETH(),
+            address(0), //token (unused)
+            0,
+            "lfg"
+        );
+
+        // Funds leaving the ecosystem -> fee taken
+        assertEq(
+            address(_terminal3_2).balance,
+            initTerminalBalance - PRBMath.mulDiv(1 ether, jbLibraries().MAX_FEE(), jbLibraries().MAX_FEE() + _terminal3_2.fee())
+        );
+
+        // Price for the amount (in USD) that is distributable based on the terminals current balance
+        uint256 distributableAmount = PRBMath.mulDiv(
+            0.5 ether,
+            10**18, // Use _MAX_FIXED_POINT_FIDELITY to keep as much of the `_amount.value`'s fidelity as possible when converting.
+            jbPrices().priceFor(jbLibraries().ETH(), jbLibraries().USD(), 18)
+        );
+
+        // Confirm that anything over the distributableAmount (0.5 eth in USD) will fail via paymentterminalstore3_2
+        // This doesn't work when expecting & calling distributePayoutsOf bc of chained calls
+        vm.prank(address(_terminal3_2));
+        vm.expectRevert(abi.encodeWithSignature("INADEQUATE_PAYMENT_TERMINAL_STORE_BALANCE()"));
+        _jbPaymentTerminalStore3_2.recordDistributionFor(2, distributableAmount + 1, 2);
+
+        // Should succeed with distributableAmount
+        vm.prank(_projectOwner);
+        _terminal3_2.distributePayoutsOf(
+            _projectId,
+            distributableAmount,
+            jbLibraries().USD(),
+            address(0), //token (unused)
+            0,
+            "lfg"
+        );
+
+        // Pay in another 0.5 ETH
+        vm.deal(_userWallet, 0.5 ether);
+        vm.prank(_userWallet);
+
+        _terminal3_2.pay{value: 0.5 ether}(
+            _projectId,
+            0.5 ether,
+            address(0),
+            _userWallet,
+            0,
+            false,
+            "Take my money!",
+            new bytes(0)
+        );
+
+        // Trying to distribute via our ETH distLimit will fail (currency is ETH or 1)
+        vm.prank(address(_terminal3_2));
+        vm.expectRevert(abi.encodeWithSignature("DISTRIBUTION_AMOUNT_LIMIT_REACHED()"));
+        _jbPaymentTerminalStore3_2.recordDistributionFor(2, 0.5 ether, 1);
+
+        // But distribution via USD limit will succeed 
+        vm.prank(_projectOwner);
+        _terminal3_2.distributePayoutsOf(
+            _projectId,
+            distributableAmount,
+            jbLibraries().USD(),
+            address(0), //token (unused)
+            0,
+            "lfg"
+        );
+        
+    }
+
+    function testFuzzedInvalidAllowanceCurrencyOrdering(uint24 ALLOWCURRENCY) external {
+        JBFundAccessConstraints3_1[] memory _fundAccessConstraints = new JBFundAccessConstraints3_1[](1);
+        JBCurrencyAmount[] memory _distributionLimits = new JBCurrencyAmount[](1);
+        JBCurrencyAmount[] memory _overflowAllowances = new JBCurrencyAmount[](2);
+
+        _distributionLimits[0] = JBCurrencyAmount({
+            value: 1,
+            currency: jbLibraries().ETH()
+        });
+
+        _overflowAllowances[0] = JBCurrencyAmount({
+            value: 1,
+            currency: ALLOWCURRENCY
+        });
+
+        _overflowAllowances[1] = JBCurrencyAmount({
+            value: 1,
+            currency: ALLOWCURRENCY == 0 ? 0 : ALLOWCURRENCY - 1
+        });
+
+        _fundAccessConstraints[0] = 
+            JBFundAccessConstraints3_1({
+                terminal: _terminal3_2,
+                token: jbLibraries().ETHToken(),
+                distributionLimits: _distributionLimits,
+                overflowAllowances: _overflowAllowances
+            });
+
+        JBFundingCycleConfiguration[] memory _cycleConfig = new JBFundingCycleConfiguration[](1);
+
+        _cycleConfig[0].mustStartAtOrAfter = 0;
+        _cycleConfig[0].data = _data;
+        _cycleConfig[0].metadata = _metadata;
+        _cycleConfig[0].groupedSplits = _groupedSplits;
+        _cycleConfig[0].fundAccessConstraints = _fundAccessConstraints;
+
+        _projectOwner = multisig();
+
+        vm.prank(_projectOwner);
+
+        vm.expectRevert(abi.encodeWithSignature("INVALID_OVERFLOW_ALLOWANCE_CURRENCY_ORDERING()"));
+        
+        _projectId = _controller.launchProjectFor(
+            _projectOwner,
+            _projectMetadata,
+            _cycleConfig,
+            _terminals,
+            ""
+        );
+    }
+
+    function testFuzzedInvalidDistCurrencyOrdering(uint24 DISTCURRENCY) external {
+        JBFundAccessConstraints3_1[] memory _fundAccessConstraints = new JBFundAccessConstraints3_1[](1);
+        JBCurrencyAmount[] memory _distributionLimits = new JBCurrencyAmount[](2);
+        JBCurrencyAmount[] memory _overflowAllowances = new JBCurrencyAmount[](1);
+
+        _distributionLimits[0] = JBCurrencyAmount({
+            value: 1,
+            currency: DISTCURRENCY
+        });
+
+        _distributionLimits[1] = JBCurrencyAmount({
+            value: 1,
+            currency: DISTCURRENCY == 0 ? 0 : DISTCURRENCY - 1
+        });
+
+        _overflowAllowances[0] = JBCurrencyAmount({
+            value: 1,
+            currency: 1
+        });
+
+        _fundAccessConstraints[0] = 
+            JBFundAccessConstraints3_1({
+                terminal: _terminal3_2,
+                token: jbLibraries().ETHToken(),
+                distributionLimits: _distributionLimits,
+                overflowAllowances: _overflowAllowances
+            });
+
+        JBFundingCycleConfiguration[] memory _cycleConfig = new JBFundingCycleConfiguration[](1);
+
+        _cycleConfig[0].mustStartAtOrAfter = 0;
+        _cycleConfig[0].data = _data;
+        _cycleConfig[0].metadata = _metadata;
+        _cycleConfig[0].groupedSplits = _groupedSplits;
+        _cycleConfig[0].fundAccessConstraints = _fundAccessConstraints;
+
+        _projectOwner = multisig();
+
+        vm.prank(_projectOwner);
+
+        vm.expectRevert(abi.encodeWithSignature("INVALID_DISTRIBUTION_LIMIT_CURRENCY_ORDERING()"));
+        
+        _projectId = _controller.launchProjectFor(
+            _projectOwner,
+            _projectMetadata,
+            _cycleConfig,
+            _terminals,
+            ""
+        );
+    }
+
     function testFuzzedConfigureAccess(uint232 DISTLIMIT, uint232 ALLOWLIMIT, uint256 DISTCURRENCY, uint256 ALLOWCURRENCY) external {
         DISTCURRENCY = bound(uint256(DISTCURRENCY), uint256(0), type(uint24).max - 1);
         ALLOWCURRENCY = bound(uint256(ALLOWCURRENCY), uint256(0), type(uint24).max - 1);
@@ -238,9 +486,6 @@ contract TestMultipleDistLimits_Local is TestBaseWorkflow {
             address(_terminal3_2).balance,
             initTerminalBalance - PRBMath.mulDiv(distributedAmount, jbLibraries().MAX_FEE(), jbLibraries().MAX_FEE() + _terminal3_2.fee())
         );
-        
-        // Forge doesn't like this expectation, it's hella buggy, but this is going to fail with inad balance..
-        /* vm.expectRevert(abi.encodeWithSignature("INADEQUATE_PAYMENT_TERMINAL_STORE_BALANCE()")); */
 
         // First dist should be fine based on price
         vm.prank(_projectOwner);
