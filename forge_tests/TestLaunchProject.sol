8721629c6fe0785b646ccfd49c3aca49330ee64f
nowonder <justinpulley92@gmail.com>
2023-11-14 23:01:38 +0000

    launch projects port

---
+++ forge_tests/TestLaunchProject.sol
@@ -0,0 +1,140 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.6;
+
+import /* {*} from */ "./helpers/TestBaseWorkflow.sol";
+
+// Projects can be launched.
+contract TestLaunchProject_Local is TestBaseWorkflow {
+    IJBController3_1 private _controller;
+    JBProjectMetadata private _projectMetadata;
+    JBFundingCycleData private _data;
+    JBFundingCycleMetadata private _metadata;
+    JBGroupedSplits[] private _groupedSplits;
+    JBFundAccessConstraints[] private _fundAccessConstraints;
+    IJBPaymentTerminal private _terminal;
+
+    address private _projectOwner;
+
+    function setUp() public override {
+        super.setUp();
+
+        _projectOwner = multisig();
+        _terminal = jbPayoutRedemptionTerminal();
+        _controller = jbController();
+
+        _projectMetadata = JBProjectMetadata({content: "myIPFSHash", domain: 1});
+        _data = JBFundingCycleData({
+            duration: 0,
+            weight: 0,
+            discountRate: 0,
+            ballot: IJBFundingCycleBallot(address(0))
+        });
+
+        _metadata = JBFundingCycleMetadata({
+            global: JBGlobalFundingCycleMetadata({
+                allowSetTerminals: false,
+                allowSetController: false,
+                pauseTransfers: false
+            }),
+            reservedRate: 0,
+            redemptionRate: 0,
+            baseCurrency: uint32(uint160(JBTokens.ETH)),
+            pausePay: false,
+            allowMinting: false,
+            allowTerminalMigration: false,
+            allowControllerMigration: false,
+            holdFees: false,
+            useTotalOverflowForRedemptions: false,
+            useDataSourceForPay: false,
+            useDataSourceForRedeem: false,
+            dataSource: address(0),
+            metadata: 0
+        });
+    }
+
+    function testLaunchProject() public {
+        // Package up cycle config.
+        JBFundingCycleConfig[] memory _cycleConfig = new JBFundingCycleConfig[](1);
+        _cycleConfig[0].mustStartAtOrAfter = 0;
+        _cycleConfig[0].data = _data;
+        _cycleConfig[0].metadata = _metadata;
+        _cycleConfig[0].groupedSplits = new JBGroupedSplits[](0);
+        _cycleConfig[0].fundAccessConstraints = new JBFundAccessConstraints[](0);
+
+        // Package up terminal config.
+        JBTerminalConfig[] memory _terminalConfigurations = new JBTerminalConfig[](1);
+        JBAccountingContextConfig[] memory _accountingContexts = new JBAccountingContextConfig[](1);
+        _accountingContexts[0] =
+            JBAccountingContextConfig({token: JBTokens.ETH, standard: JBTokenStandards.NATIVE});
+        _terminalConfigurations[0] =
+            JBTerminalConfig({terminal: _terminal, accountingContextConfigs: _accountingContexts});
+
+        uint256 projectId = _controller.launchProjectFor({
+            owner: _projectOwner,
+            projectMetadata: _projectMetadata,
+            fundingCycleConfigurations: _cycleConfig,
+            terminalConfigurations: _terminalConfigurations,
+            memo: ""
+        });
+
+        // Get a reference to the first funding cycle.
+        JBFundingCycle memory fundingCycle = jbFundingCycleStore().currentOf(projectId);
+
+        // Make sure the funding cycle got saved correctly.
+        assertEq(fundingCycle.number, 1);
+        assertEq(fundingCycle.weight, _data.weight);
+    }
+
+    function testLaunchProjectFuzzWeight(uint256 _weight) public {
+        uint256 _projectId;
+        
+        _data = JBFundingCycleData({
+            duration: 14,
+            weight: _weight,
+            discountRate: 450_000_000,
+            ballot: IJBFundingCycleBallot(address(0))
+        });
+
+        // Package up cycle config.
+        JBFundingCycleConfig[] memory _cycleConfig = new JBFundingCycleConfig[](1);
+        _cycleConfig[0].mustStartAtOrAfter = 0;
+        _cycleConfig[0].data = _data;
+        _cycleConfig[0].metadata = _metadata;
+        _cycleConfig[0].groupedSplits = new JBGroupedSplits[](0);
+        _cycleConfig[0].fundAccessConstraints = new JBFundAccessConstraints[](0);
+
+        // Package up terminal config.
+        JBTerminalConfig[] memory _terminalConfigurations = new JBTerminalConfig[](1);
+        JBAccountingContextConfig[] memory _accountingContexts = new JBAccountingContextConfig[](1);
+        _accountingContexts[0] =
+            JBAccountingContextConfig({token: JBTokens.ETH, standard: JBTokenStandards.NATIVE});
+        _terminalConfigurations[0] =
+            JBTerminalConfig({terminal: _terminal, accountingContextConfigs: _accountingContexts});
+
+        // expectRevert on the next call if weight overflowing
+        if (_weight > type(uint88).max) {
+            vm.expectRevert(abi.encodeWithSignature("INVALID_WEIGHT()"));
+
+            _projectId = _controller.launchProjectFor({
+            owner: _projectOwner,
+            projectMetadata: _projectMetadata,
+            fundingCycleConfigurations: _cycleConfig,
+            terminalConfigurations: _terminalConfigurations,
+            memo: ""
+        });
+        } else {
+            _projectId = _controller.launchProjectFor({
+            owner: _projectOwner,
+            projectMetadata: _projectMetadata,
+            fundingCycleConfigurations: _cycleConfig,
+            terminalConfigurations: _terminalConfigurations,
+            memo: ""
+        });
+
+            JBFundingCycle memory fundingCycle = jbFundingCycleStore().currentOf(_projectId);
+
+            assertEq(fundingCycle.number, 1);
+            assertEq(fundingCycle.weight, _weight);
+        }
+    }
+}
