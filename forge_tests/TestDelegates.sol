0e4be2cd0e66ecac66e9d00d003ffa4f940ac623
mejango <mejango@protonmail.com>
2023-11-14 16:02:42 -0500

    pay delegate call works

--- forge_tests/TestDelegates.sol
+++ forge_tests/TestDelegates.sol
@@ -4,43 +4,39 @@ pragma solidity >=0.8.6;
 import /* {*} from */ "./helpers/TestBaseWorkflow.sol";
 
 contract TestDelegates_Local is TestBaseWorkflow {
-    uint256 private _ethCurrency; 
+    uint8 private constant _WEIGHT_DECIMALS = 18; // FIXED
+    uint8 private constant _NATIVE_TOKEN_DECIMALS = 18; // FIXED
+    uint256 private constant _WEIGHT = 1000 * 10 ** _WEIGHT_DECIMALS;
+    uint256 private constant _DATA_SOURCE_WEIGHT = 2000 * 10 ** _WEIGHT_DECIMALS;
+    address private constant _DATA_SOURCE = address(bytes20(keccak256("datasource")));
+    bytes private constant _PAYER_METADATA = bytes("Some payer metadata");
+
     IJBController3_1 private _controller;
-    IJBMultiTerminal private __terminal;
     IJBPrices private _prices;
     JBTokenStore private _tokenStore;
-    
-    JBProjectMetadata private _projectMetadata;
     JBFundingCycleData private _data;
     JBFundingCycleMetadata _metadata;
-    JBGroupedSplits[] private _groupedSplits;
-    IJBPaymentTerminal[] private _terminals;
+    IJBPaymentTerminal private _terminal;
     address private _projectOwner;
     address private _beneficiary;
-
-    address _datasource = address(bytes20(keccak256("datasource")));
+    address private _payer;
 
     uint256 _projectId;
-
-    uint256 WEIGHT = 1000 * 10 ** 18;
 
     function setUp() public override {
         super.setUp();
 
-        uint256 _ethDistributionLimit = 1 ether;
-        _ethCurrency = uint32(uint160(JBTokens.ETH));
         _controller = jbController();
         _projectOwner = multisig();
         _beneficiary = beneficiary();
+        _payer = address(1_234_567);
         _prices = jbPrices();
-        __terminal = jbPayoutRedemptionTerminal();
+        _terminal = jbPayoutRedemptionTerminal();
         _tokenStore = jbTokenStore();
 
-        _projectMetadata = JBProjectMetadata({content: "myIPFSHash", domain: 1});
-
         _data = JBFundingCycleData({
-            duration: 14,
-            weight: 1000 * 10 ** 18,
+            duration: 0,
+            weight: _WEIGHT,
             discountRate: 0,
             ballot: IJBFundingCycleBallot(address(0))
         });
@@ -51,8 +47,8 @@ contract TestDelegates_Local is TestBaseWorkflow {
                 allowSetController: false,
                 pauseTransfers: false
             }),
-            reservedRate: JBConstants.MAX_RESERVED_RATE / 2,
-            redemptionRate: JBConstants.MAX_RESERVED_RATE / 2,
+            reservedRate: 0,
+            redemptionRate: 0,
             baseCurrency: uint32(uint160(JBTokens.ETH)),
             pausePay: false,
             allowMinting: false,
@@ -62,173 +58,148 @@ contract TestDelegates_Local is TestBaseWorkflow {
             useTotalOverflowForRedemptions: false,
             useDataSourceForPay: true,
             useDataSourceForRedeem: true,
-            dataSource: _datasource,
+            dataSource: _DATA_SOURCE,
             metadata: 0
         });
-
-        // Package up fund access constraints
-        JBFundAccessConstraints[] memory _fundAccessConstraints = new JBFundAccessConstraints[](1);
-        JBCurrencyAmount[] memory _distributionLimits = new JBCurrencyAmount[](1);
-        JBCurrencyAmount[] memory _overflowAllowances = new JBCurrencyAmount[](1);
-        
-        _distributionLimits[0] = JBCurrencyAmount({
-            value: _ethDistributionLimit,
-            currency: uint32(uint160(JBTokens.ETH))
-        });
-        _overflowAllowances[0] = JBCurrencyAmount({
-            value: 1 ether,
-            currency: uint32(uint160(JBTokens.ETH))
-        });
-        _fundAccessConstraints[0] = 
-            JBFundAccessConstraints({
-                terminal: __terminal,
-                token: JBTokens.ETH,
-                distributionLimits: _distributionLimits,
-                overflowAllowances: _overflowAllowances
-            });
 
         // Package up cycle config.
         JBFundingCycleConfig[] memory _cycleConfig = new JBFundingCycleConfig[](1);
         _cycleConfig[0].mustStartAtOrAfter = 0;
         _cycleConfig[0].data = _data;
         _cycleConfig[0].metadata = _metadata;
-        _cycleConfig[0].groupedSplits = _groupedSplits;
-        _cycleConfig[0].fundAccessConstraints = _fundAccessConstraints;
+        _cycleConfig[0].groupedSplits = new JBGroupedSplits[](0);
+        _cycleConfig[0].fundAccessConstraints = new JBFundAccessConstraints[](0);
 
         JBTerminalConfig[] memory _terminalConfigurations = new JBTerminalConfig[](1);
-            JBAccountingContextConfig[] memory _accountingContexts = new JBAccountingContextConfig[](1);
-            _accountingContexts[0] = JBAccountingContextConfig({
-                token: JBTokens.ETH,
-                standard: JBTokenStandards.NATIVE
-            });
-            /* _accountingContexts[1] = JBAccountingContextConfig({
-                token: address(usdcToken()),
-                standard: JBTokenStandards.ERC20
-            }); */
-            _terminalConfigurations[0] = JBTerminalConfig({
-                terminal: __terminal,
-                accountingContextConfigs: _accountingContexts
-            });
-
-        _terminals.push(__terminal);
-
-        /* // dummy
-        _controller.launchProjectFor({
-            owner: _projectOwner,
-            projectMetadata: _projectMetadata,
-            fundingCycleConfigurations: _cycleConfig,
-            terminalConfigurations: _terminalConfigurations,
-            memo: ""
-        }); */
+        JBAccountingContextConfig[] memory _accountingContexts = new JBAccountingContextConfig[](1);
+        _accountingContexts[0] =
+            JBAccountingContextConfig({token: JBTokens.ETH, standard: JBTokenStandards.NATIVE});
+        _terminalConfigurations[0] =
+            JBTerminalConfig({terminal: _terminal, accountingContextConfigs: _accountingContexts});
 
         _projectId = _controller.launchProjectFor({
             owner: _projectOwner,
-            projectMetadata: _projectMetadata,
+            projectMetadata: JBProjectMetadata({content: "myIPFSHash", domain: 1}),
             fundingCycleConfigurations: _cycleConfig,
             terminalConfigurations: _terminalConfigurations,
             memo: ""
         });
     }
 
-    function testPayDelegates(uint256 _numberOfAllocations, uint256 _totalToAllocate) public {
-        _numberOfAllocations = bound(_numberOfAllocations, 1, 5);
+    function testPayDelegates(uint256 _numberOfAllocations, uint256 _ethPayAmount) public {
+        // Bound the number of allocations to a reasonable amount.
+        _numberOfAllocations = bound(_numberOfAllocations, 1, 2);
+        // Make sure the amount of tokens generated fits in a register, and that each allocation can get some.
+        _ethPayAmount =
+            bound(_ethPayAmount, _numberOfAllocations, type(uint256).max / _DATA_SOURCE_WEIGHT);
 
+        // epa * weight / epad < max*epad/weight
+
+        // Keep a reference to the allocations.
         JBPayDelegateAllocation3_1_1[] memory _allocations =
             new JBPayDelegateAllocation3_1_1[](_numberOfAllocations);
-        uint256[] memory payDelegateAmounts = new uint256[](_numberOfAllocations);
 
-        _beneficiary = address(bytes20(keccak256("beneficiary")));
+        // Keep a refernce to the amounts that'll be allocated.
+        uint256[] memory _payDelegateAmounts = new uint256[](_numberOfAllocations);
 
-        // Check that we are not going to overflow uint256 and calculate the total pay amount
-        _totalToAllocate = bound(_totalToAllocate, payDelegateAmounts.length, type(uint224).max - 1);
-        uint256 _paySum = _totalToAllocate;
+        // Keep a reference to the amount that'll be paid and allocated.
+        uint256 _totalToAllocate = _ethPayAmount;
 
-        // Allocate descending amounts (by half)
-        for (uint256 i; i < payDelegateAmounts.length - 1; i++) {
-            payDelegateAmounts[i] = _totalToAllocate / (payDelegateAmounts.length * 2);
-            _totalToAllocate -= payDelegateAmounts[i];
+        // Spread the paid amount through all allocations, in various chunks, omitted the last entry.
+        for (uint256 i; i < _numberOfAllocations - 1; i++) {
+            _payDelegateAmounts[i] = _totalToAllocate / (_payDelegateAmounts.length * 2);
+            _totalToAllocate -= _payDelegateAmounts[i];
         }
 
-        // Rest to allocate into the last allocations
-        payDelegateAmounts[payDelegateAmounts.length - 1] = _totalToAllocate;
+        // Send the rest to the last entry.
+        _payDelegateAmounts[_payDelegateAmounts.length - 1] = _totalToAllocate;
 
-        (JBFundingCycle memory fundingCycle,) = _controller.currentFundingCycleOf(_projectId);
-        for (uint256 i = 0; i < payDelegateAmounts.length; i++) {
+        // Keep a reference to the current funding cycle.
+        (JBFundingCycle memory _fundingCycle,) = _controller.currentFundingCycleOf(_projectId);
+
+        // Iterate through each allocation.
+        for (uint256 i = 0; i < _numberOfAllocations; i++) {
+            // Make up an address for the delegate.
             address _delegateAddress =
                 address(bytes20(keccak256(abi.encodePacked("PayDelegate", i))));
 
+            // Send along some metadata to the pay delegate.
+            bytes memory _dataSourceMetadata = bytes("Some data source metadata");
+
+            // Package up the delegate allocation struct.
             _allocations[i] = JBPayDelegateAllocation3_1_1(
-                IJBPayDelegate3_1_1(_delegateAddress), payDelegateAmounts[i], bytes("")
+                IJBPayDelegate3_1_1(_delegateAddress), _payDelegateAmounts[i], _dataSourceMetadata
             );
 
-            JBDidPayData3_1_1 memory _didPayData = JBDidPayData3_1_1(
-                _beneficiary,
-                _projectId,
-                fundingCycle.configuration,
-                JBTokenAmount(
+            // Keep a reference to the data that'll be received by the delegate.
+            JBDidPayData3_1_1 memory _didPayData = JBDidPayData3_1_1({
+                payer: _payer,
+                projectId: _projectId,
+                currentFundingCycleConfiguration: _fundingCycle.configuration,
+                amount: JBTokenAmount(
                     JBTokens.ETH,
-                    _paySum,
-                    JBMultiTerminal(address(_terminals[0])).accountingContextForTokenOf(_projectId, JBTokens.ETH).decimals,
-                    JBMultiTerminal(address(_terminals[0])).accountingContextForTokenOf(_projectId, JBTokens.ETH).currency
-                ),
-                JBTokenAmount(
+                    _ethPayAmount,
+                    _terminal.accountingContextForTokenOf(_projectId, JBTokens.ETH).decimals,
+                    _terminal.accountingContextForTokenOf(_projectId, JBTokens.ETH).currency
+                    ),
+                forwardedAmount: JBTokenAmount(
                     JBTokens.ETH,
-                    payDelegateAmounts[i],
-                    JBMultiTerminal(address(_terminals[0])).accountingContextForTokenOf(_projectId, JBTokens.ETH).decimals,
-                    JBMultiTerminal(address(_terminals[0])).accountingContextForTokenOf(_projectId, JBTokens.ETH).currency
-                ),
-                0,
-                0,
-                _beneficiary,
-                "",
-                new bytes(0) // empty metadata
-            );
+                    _payDelegateAmounts[i],
+                    _terminal.accountingContextForTokenOf(_projectId, JBTokens.ETH).decimals,
+                    _terminal.accountingContextForTokenOf(_projectId, JBTokens.ETH).currency
+                    ),
+                weight: _WEIGHT,
+                projectTokenCount: PRBMath.mulDiv(
+                    _ethPayAmount, _DATA_SOURCE_WEIGHT, 10 ** _NATIVE_TOKEN_DECIMALS
+                    ),
+                beneficiary: _beneficiary,
+                dataSourceMetadata: _dataSourceMetadata,
+                payerMetadata: _PAYER_METADATA
+            });
 
             // Mock the delegate
             vm.mockCall(
-                _delegateAddress, abi.encodeWithSelector(IJBPayDelegate3_1_1.didPay.selector), ""
+                _delegateAddress,
+                abi.encodeWithSelector(IJBPayDelegate3_1_1.didPay.selector),
+                abi.encode(_didPayData)
             );
 
             // Assert that the delegate gets called with the expected value
             vm.expectCall(
                 _delegateAddress,
-                payDelegateAmounts[i],
+                _payDelegateAmounts[i],
                 abi.encodeWithSelector(IJBPayDelegate3_1_1.didPay.selector, _didPayData)
             );
 
-            /* // Expect an event to be emitted for every delegate
+            // Expect an event to be emitted for every delegate
             vm.expectEmit(true, true, true, true);
             emit DelegateDidPay(
                 IJBPayDelegate3_1_1(_delegateAddress),
                 _didPayData,
-                payDelegateAmounts[i],
-                _beneficiary
-            ); */
+                _payDelegateAmounts[i],
+                _payer
+            );
         }
 
         vm.mockCall(
-            _datasource,
+            _DATA_SOURCE,
             abi.encodeWithSelector(IJBFundingCycleDataSource3_1_1.payParams.selector),
-            abi.encode(
-                0, // weight
-                "", // memo
-                _allocations // allocations
-            )
+            abi.encode(_DATA_SOURCE_WEIGHT, _allocations)
         );
 
-        vm.deal(_beneficiary, _paySum);
-        vm.prank(_beneficiary);
+        vm.deal(_payer, _ethPayAmount);
+        vm.prank(_payer);
 
         // Pay the project such that the _beneficiary receives project tokens.
-        __terminal.pay{value: _paySum}({
-            projectId: _projectId, 
-            amount: _paySum, 
-            token: JBTokens.ETH, 
-            beneficiary: _beneficiary, 
-            minReturnedTokens: 0, 
+        _terminal.pay{value: _ethPayAmount}({
+            projectId: _projectId,
+            amount: _ethPayAmount,
+            token: JBTokens.ETH,
+            beneficiary: _beneficiary,
+            minReturnedTokens: 0,
             memo: "Forge Test",
-            metadata: new bytes(0)
-        }); 
+            metadata: _PAYER_METADATA
+        });
     }
 
     event DelegateDidPay(
